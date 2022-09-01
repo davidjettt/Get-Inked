cfaeadbe24ec6078d58993d6d190cd52f0931e23
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-09-01 01:04:49 -0700

    added home page component with main image

---
+++ react-app/src/components/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+
+
+export default function HomePage() {
+    const mainHomePageImage = 'https://res.cloudinary.com/dtjyf5kpn/image/upload/v1662018933/Stefanie_6-e1619631407239_e0wz9n_653ea0-landscape.jpg'
+
+    return (
+        <>
+            <div>
+                <img src={mainHomePageImage} alt='traditional-japanese-tattoos-girl' />
+            </div>
+        </>
+    )
+}
