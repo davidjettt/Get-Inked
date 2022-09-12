2429e8610c0ee040b30244ee169379ccd1b9a9cb
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-09-11 19:16:43 -0700

    added max length for description fields, npm i caniuse-lite package

--- app/forms/tattoo_form.py
+++ app/forms/tattoo_form.py
@@ -7,7 +7,12 @@ def tattoo_style_length(form, field):
     if len(tattoo_style) > 20:
         raise ValidationError('Tattoo style must not exceed over 20 characters')
 
+def description_length(form, field):
+    description = field.data
+    if len(description) > 500:
+        raise ValidationError('Description cannot exceed over 500 characters')
+
 
 class TattooForm(FlaskForm):
-    description = StringField('description', validators=[DataRequired(message='A tattoo description is required')])
+    description = StringField('description', validators=[DataRequired(message='A tattoo description is required'), description_length])
     tattoo_style = StringField('tattoo style', validators=[DataRequired(message='A tattoo style is required'), tattoo_style_length])
