b6f41a404d0a4eff94b1a03eacfcc1d50ba46499
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-12-22 23:56:32 -0800

    changed offest increment

--- react-app/src/components/Tattoos/Tattoos.js
+++ react-app/src/components/Tattoos/Tattoos.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import {  useSelector } from "react-redux";
+// import {  useSelector } from "react-redux";
 import SingleTattooModal from "./SingleTattooModal";
 import './Tattoos.css'
 
